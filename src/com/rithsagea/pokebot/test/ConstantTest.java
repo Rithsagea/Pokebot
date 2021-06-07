522099f7871d52ff7a9cce6975a9c4e6feef5f0b
Rithsagea <rithsagea@gmail.com>
2021-06-07 19:48:06 -0400

    added growth rates for exp gain

---
+++ src/com/rithsagea/pokebot/test/ConstantTest.java
@@ -0,0 +1,9 @@
+package com.rithsagea.pokebot.test;
+
+import com.rithsagea.pokebot.types.GrowthRate;
+
+public class ConstantTest {
+	public static void main(String[] args) {
+		System.out.println(GrowthRate.getExperience(GrowthRate.ERRATIC, 100));
+	}
+}
---
+++ src/com/rithsagea/pokebot/types/GrowthRate.java
@@ -0,0 +1,37 @@
+package com.rithsagea.pokebot.types;
+
+import java.util.List;
+
+import com.rithsagea.pokebot.util.CSVUtil;
+
+public class GrowthRate {
+	
+	private static final String EXPERIENCE_CSV = "resources/csv/table/experience.csv";
+	
+	public static final int SLOW = 1;
+	
+	public static final int MEDIUM_FAST = 2;
+	
+	public static final int FAST = 3;
+	
+	public static final int MEDIUM_SLOW = 4;
+	
+	public static final int ERRATIC = 5;
+	
+	public static final int FLUCTUATING = 6;
+	
+	private static final int EXPERIENCE_TABLE[][] = new int[6][100];
+	static {
+		List<String[]> data = CSVUtil.readFile(EXPERIENCE_CSV);
+		data.remove(0);
+		for(int x = 0; x < 100; x++) {
+			for(int y = 0; y < 6; y++) {
+				EXPERIENCE_TABLE[y][x] = Integer.parseInt(data.get(x)[y + 1]);
+			}
+		}
+	}
+	
+	public static int getExperience(int growthRate, int level) {
+		return EXPERIENCE_TABLE[growthRate - 1][level - 1];
+	}
+}
