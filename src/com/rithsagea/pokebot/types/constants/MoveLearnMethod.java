f6d48554b0b88b744e792922243223384bbd227d
Rithsagea Aquadom <rithsagea@gmail.com>
2021-06-11 11:08:30 -0400

    redid movesets

---
+++ src/com/rithsagea/pokebot/types/constants/MoveLearnMethod.java
@@ -0,0 +1,13 @@
+package com.rithsagea.pokebot.types.constants;
+
+public class MoveLearnMethod {
+	public static final int LEVEL_UP = 1;
+	public static final int EGG = 2;
+	public static final int TUTOR = 3;
+	public static final int MACHINE = 4;
+	public static final int STADIUM_SURFING = 5;
+	public static final int LIGHT_BALL_EGG = 6;
+	//7-11 doesn't exist, but just in case
+//	public static final int FORM_CHANGE = 10;
+//	public static final int ZYGARDE_CUBE = 11;
+}
--- src/com/rithsagea/pokebot/types/registry/LearnedMove.java
+++ src/com/rithsagea/pokebot/types/registry/LearnedMove.java
@@ -1,14 +1,15 @@
 package com.rithsagea.pokebot.types.registry;
 
+import com.rithsagea.pokebot.Registry;
 import com.rithsagea.pokebot.Util;
 
 public class LearnedMove {
-	public final int move_id;
+	public final RegistryMove move;
 	public final int pokemon_move_method_id;
 	public final int level;
 	
 	public LearnedMove(String[] s) {
-		move_id = Util.parseInt(s[2]);
+		move = Registry.getMove(Util.parseInt(s[2]));
 		pokemon_move_method_id = Util.parseInt(s[3]);
 		level = Util.parseInt(s[4]);
 	}
